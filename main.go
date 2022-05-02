debd4010dfaf4a5718187f2bdce043375936badc
Alexander Kluge <akluge201@gmail.com>
2022-05-02 17:06:55 +0200

    WIP

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 
+	corev1 "k8s.io/api/core/v1"
 	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	//"k8s.io/client-go/kubernetes"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
 )
+
+type Zone struct {
+	UUID    string   `json:"id"`
+	Name    string   `json:"name"`
+	Type    string   `json:"type"`
+	Records []Record `json:"records"`
+}
+
+type Record struct {
+	UUID     string `json:"id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Type     string `json:"type,omitempty"`
+	Content  string `json:"content,omitempty"`
+	TTL      int    `json:"ttl,omitempty"`
+	Priority int    `json:"prio"`
+	Disabled bool   `json:"disabled"`
+}
 
 var GroupName = os.Getenv("GROUP_NAME")
 
@@ -20,52 +44,21 @@ func main() {
 		panic("GROUP_NAME must be specified")
 	}
 
-	// This will register our custom DNS provider with the webhook serving
-	// library, making it available as an API under the provided GroupName.
-	// You can register multiple DNS provider implementations with a single
-	// webhook, where the Name() method will be used to disambiguate between
-	// the different implementations.
 	cmd.RunWebhookServer(GroupName,
-		&customDNSProviderSolver{},
+		&ionosDNSProviderSolver{},
 	)
 }
 
-// customDNSProviderSolver implements the provider-specific logic needed to
-// 'present' an ACME challenge TXT record for your own DNS provider.
-// To do so, it must implement the `github.com/jetstack/cert-manager/pkg/acme/webhook.Solver`
-// interface.
-type customDNSProviderSolver struct {
-	// If a Kubernetes 'clientset' is needed, you must:
-	// 1. uncomment the additional `client` field in this structure below
-	// 2. uncomment the "k8s.io/client-go/kubernetes" import at the top of the file
-	// 3. uncomment the relevant code in the Initialize method below
-	// 4. ensure your webhook's service account has the required RBAC role
-	//    assigned to it for interacting with the Kubernetes APIs you need.
-	//client kubernetes.Clientset
+type ionosDNSProviderSolver struct {
+	client *kubernetes.Clientset
 }
 
-// customDNSProviderConfig is a structure that is used to decode into when
-// solving a DNS01 challenge.
-// This information is provided by cert-manager, and may be a reference to
-// additional configuration that's needed to solve the challenge for this
-// particular certificate or issuer.
-// This typically includes references to Secret resources containing DNS
-// provider credentials, in cases where a 'multi-tenant' DNS solver is being
-// created.
-// If you do *not* require per-issuer or per-certificate configuration to be
-// provided to your webhook, you can skip decoding altogether in favour of
-// using CLI flags or similar to provide configuration.
-// You should not include sensitive information here. If credentials need to
-// be used by your provider here, you should reference a Kubernetes Secret
-// resource and fetch these credentials using a Kubernetes clientset.
-type customDNSProviderConfig struct {
-	// Change the two fields below according to the format of the configuration
-	// to be decoded.
-	// These fields will be set by users in the
-	// `issuer.spec.acme.dns01.providers.webhook.config` field.
-
-	//Email           string `json:"email"`
-	//APIKeySecretRef v1alpha1.SecretKeySelector `json:"apiKeySecretRef"`
+type ionosDNSProviderConfig struct {
+	Endpoint         string                   `json:"endpoint"`
+	AuthAPIKey       string                   `json:"authApiKey"`
+	AuthAPISecretRef corev1.SecretKeySelector `json:"authApiSecretRef"`
+	BaseURL          string                   `json:"baseUrl"`
+	TTL              int                      `json:"ttl"`
 }
 
 // Name is used as the name for this DNS solver when referencing it on the ACME
@@ -74,8 +67,8 @@ type customDNSProviderConfig struct {
 // solvers configured with the same Name() **so long as they do not co-exist
 // within a single webhook deployment**.
 // For example, `cloudflare` may be used as the name of a solver.
-func (c *customDNSProviderSolver) Name() string {
-	return "my-custom-solver"
+func (c *ionosDNSProviderSolver) Name() string {
+	return "ionos"
 }
 
 // Present is responsible for actually presenting the DNS record with the
@@ -83,16 +76,50 @@ func (c *customDNSProviderSolver) Name() string {
 // This method should tolerate being called multiple times with the same value.
 // cert-manager itself will later perform a self check to ensure that the
 // solver has correctly configured the DNS provider.
-func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+func (c *ionosDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	cfg, err := loadConfig(ch.Config)
 	if err != nil {
 		return err
 	}
 
-	// TODO: do something more useful with the decoded configuration
-	fmt.Printf("Decoded configuration %v", cfg)
+	record := &Record{
+		Name:     ch.ResolvedFQDN,
+		Type:     "TXT",
+		Content:  ch.Key,
+		TTL:      *&cfg.TTL,
+		Priority: 0,
+		Disabled: false,
+	}
 
-	// TODO: add code that sets a record in the DNS provider's console
+	recordArray := []Record{*record}
+
+	jsonValue, err := json.Marshal(recordArray)
+	body := bytes.NewBuffer(jsonValue)
+
+	fmt.Println(body)
+
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, ch.Endpoint+"/v1/zones/"+ch.ResolvedZone+"/records", body)
+
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add(HEADER_NAME, PREFIX+"."+SECRET)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+
+	if err != nil || resp.StatusCode != 201 {
+		return
+	}
+
 	return nil
 }
 
@@ -102,9 +129,59 @@ func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 // value provided on the ChallengeRequest should be cleaned up.
 // This is in order to facilitate multiple DNS validations for the same domain
 // concurrently.
-func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-	// TODO: add code that deletes a record from the DNS provider's console
+func (c *ionosDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodDelete, cfg.BaseURL+"/v1/zones/"+zone+"/records/"+record_id, nil)
+
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add(HEADER_NAME, PREFIX+"."+SECRET)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+
+	fmt.Println(resp.StatusCode)
+
+	if err != nil || resp.StatusCode != 200 {
+		return nil
+	}
 	return nil
+}
+
+func (c *ionosDNSProviderSolver) validateAndGetSecret(cfg *ionosDNSProviderConfig, namespace string) (string, error) {
+	fmt.Printf("validateAndGetSecret...")
+	// Check that the host is defined
+	if cfg.AuthAPIKey != "" {
+		return cfg.AuthAPIKey, nil
+	}
+
+	// Try to load the API key
+	if cfg.AuthAPISecretRef.LocalObjectReference.Name == "" {
+		return "", errors.New("No Arvan API secret provided")
+	}
+
+	sec, err := c.client.CoreV1().Secrets(namespace).Get(context.TODO(), cfg.AuthAPISecretRef.LocalObjectReference.Name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	secBytes, ok := sec.Data[cfg.AuthAPISecretRef.Key]
+	if !ok {
+		return "", fmt.Errorf("Key %q not found in secret \"%s/%s\"", cfg.AuthAPISecretRef.Key, cfg.AuthAPISecretRef.LocalObjectReference.Name, namespace)
+	}
+
+	apiKey := string(secBytes)
+
+	return apiKey, nil
 }
 
 // Initialize will be called when the webhook first starts.
@@ -116,25 +193,21 @@ func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
 // provider accounts.
 // The stopCh can be used to handle early termination of the webhook, in cases
 // where a SIGTERM or similar signal is sent to the webhook process.
-func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
-	///// UNCOMMENT THE BELOW CODE TO MAKE A KUBERNETES CLIENTSET AVAILABLE TO
-	///// YOUR CUSTOM DNS PROVIDER
+func (c *ionosDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	cl, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		return err
+	}
 
-	//cl, err := kubernetes.NewForConfig(kubeClientConfig)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//c.client = cl
+	c.client = cl
 
-	///// END OF CODE TO MAKE KUBERNETES CLIENTSET AVAILABLE
 	return nil
 }
 
 // loadConfig is a small helper function that decodes JSON configuration into
 // the typed config struct.
-func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
-	cfg := customDNSProviderConfig{}
+func loadConfig(cfgJSON *extapi.JSON) (ionosDNSProviderConfig, error) {
+	cfg := ionosDNSProviderConfig{}
 	// handle the 'base case' where no configuration has been provided
 	if cfgJSON == nil {
 		return cfg, nil
@@ -144,4 +217,41 @@ func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
 	}
 
 	return cfg, nil
+}
+
+const HEADER_NAME = "X-API-Key"
+
+func (c *ionosDNSProviderSolver) findManagedZoneID(cfg *ionosDNSProviderConfig, domain string, namespace string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, cfg.BaseURL+"/v1/zones", nil)
+	if err != nil {
+		return "", err
+	}
+
+	secret, _ := c.validateAndGetSecret(cfg, namespace)
+	req.Header.Add(HEADER_NAME, cfg.AuthAPIKey+"."+secret)
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	zones := make([]Zone, 0)
+
+	err = json.NewDecoder(resp.Body).Decode(&zones)
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, element := range zones {
+		if element.Name == domain {
+			return element.UUID, nil
+		}
+	}
+
+	return "", errors.New("domain not available in account")
 }
